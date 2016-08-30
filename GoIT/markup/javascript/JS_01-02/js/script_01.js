dbb96b085927e89f1fda376ce907e8ee0c5393bb
Anna Mikhaylova <acidity84@gmail.com>
2016-08-30 16:09:27 +0300

    added finish mod_1-2

--- GoIT/markup/javascript/JS_01-02/js/script_01.js
+++ GoIT/markup/javascript/JS_01-02/js/script_01.js
@@ -1,4 +1,3 @@
-
 var arr = [];
 
 for ( var i = 0; i < 5; i++)
@@ -8,21 +7,30 @@ for ( var i = 0; i < 5; i++)
 	arr.push(name);
 
 	console.log('Имя ' + (i + 1) + ' =>', name);
-
 }
-alert('НЕ получается сравнить имена в массиве ((');
-
-
-var nameUser = prompt('Введите имя пользователя:');
-if (nameUser != arr[i])
-{
-	alert( nameUser + ', вы успешно вошли!');
-
-} else
-	{
-		alert('Пользователь не найден. ');
-
-	}
 
 console.log('----------------------');
+console.log('Entered names', arr);
+console.log('----------------------');
+
+var nameUser = prompt('Введите имя пользователя:');
+var rightName;
+
+for (var j = 0; j < 5; j++)
+{
+	if (nameUser == arr[j])
+{
+	rightName = true;
+	alert( nameUser + ', вы успешно вошли!');
+	break;
+} else
+	{
+		rightName = false;
+	}
+}
+if (rightName == false)
+{
+	alert('Пользователь не найден. ');
+}
+
 console.log('Username =>', nameUser);
