acc20840f8ffc14d14c4efeee2e331dcd695280f
Anna Mikhaylova <acidity84@gmail.com>
2016-08-31 00:47:19 +0300

    updated module_01-02

--- GoIT/markup/javascript/JS_01-02/js/script_01.js
+++ GoIT/markup/javascript/JS_01-02/js/script_01.js
@@ -13,24 +13,33 @@ console.log('----------------------');
 console.log('Entered names', arr);
 console.log('----------------------');
 
+function checkName()
+{
 var nameUser = prompt('Введите имя пользователя:');
 var rightName;
 
 for (var j = 0; j < 5; j++)
 {
-	if (nameUser == arr[j])
-{
-	rightName = true;
-	alert( nameUser + ', вы успешно вошли!');
-	break;
-} else
+	if (nameUser == '' || nameUser == null)
 	{
-		rightName = false;
-	}
+		alert('Нужно было ввести имя пользователя.');
+		// return checkName();
+
+	} else
+		if (nameUser == arr[j])
+		{
+			rightName = true;
+			alert( nameUser + ', вы успешно вошли!');
+			break;
+		} else
+			{
+				rightName = false;
+			}
 }
 if (rightName == false)
 {
 	alert('Пользователь не найден. ');
 }
-
 console.log('Username =>', nameUser);
+}
+checkName();
