d57a5dcc1833ff9e1947f842d8e44df6040c8b29
Anna Mikhaylova <acidity84@gmail.com>
2016-09-12 00:52:20 +0300

    update module 11-12_template

---
+++ GoIT/markup/javascript/JS_11-12/js/template_my.js
@@ -0,0 +1,22 @@
+$(function() {
+	var template = $('#about').html();
+
+	var information = {
+      name : 'Михайлова Анна Владимировна',
+      photo : '<img src="img/Im.jpg" height="128" alt="it is me">',
+      job : 'Выпускник ХИРЭ 10тилетней давности'
+			question : 'Хочу учить фронтенд, потому что:',
+ 			answers: ['Нужно развиваться', 'Это востребовано и перспективно', 'Нравится создавать нечто красивое и стильное'],
+			phone: 'Мой контактный телефон',
+			number: '+380668308206',
+			facebook: 'Мой профиль facebook',
+			link: ['<a href="https://www.facebook.com/anna.mikhaylova.948" target="_blank" title="lets be friend">facebook.com</a>'],
+			feedback: 'Для хорошего настроения могу сделать открыточку в технике scrapbooking *)',
+  
+	};
+
+	var content = tmpl(template, information);
+
+	$('body').append(content);
+
+});
