5280eaa19271ed181c725b73a057d64fc2fae7f2
Anna Mikhaylova <acidity84@gmail.com>
2016-09-12 02:11:19 +0300

    update module 11-12_template_my

--- GoIT/markup/javascript/JS_11-12/js/template_my.js
+++ GoIT/markup/javascript/JS_11-12/js/template_my.js
@@ -1,10 +1,10 @@
 $(function() {
 	var template = $('#about').html();
 
-	var information = {
+	var info = {
       name : 'Михайлова Анна Владимировна',
       photo : '<img src="img/Im.jpg" height="128" alt="it is me">',
-      job : 'Выпускник ХИРЭ 10тилетней давности'
+      job : 'Выпускник ХИРЭ 10тилетней давности',
 			question : 'Хочу учить фронтенд, потому что:',
  			answers: ['Нужно развиваться', 'Это востребовано и перспективно', 'Нравится создавать нечто красивое и стильное'],
 			phone: 'Мой контактный телефон',
@@ -12,10 +12,10 @@ $(function() {
 			facebook: 'Мой профиль facebook',
 			link: ['<a href="https://www.facebook.com/anna.mikhaylova.948" target="_blank" title="lets be friend">facebook.com</a>'],
 			feedback: 'Для хорошего настроения могу сделать открыточку в технике scrapbooking *)',
-  
+
 	};
 
-	var content = tmpl(template, information);
+	var content = tmpl(template, info);
 
 	$('body').append(content);
 
