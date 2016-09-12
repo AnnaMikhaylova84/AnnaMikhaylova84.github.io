f51c0c43d24cd0a1824b930625465ba9246ce732
Anna Mikhaylova <acidity84@gmail.com>
2016-09-12 11:00:09 +0300

    update script_12.js

--- GoIT/markup/javascript/JS_11-12/js/script_12.js
+++ GoIT/markup/javascript/JS_11-12/js/script_12.js
@@ -2,10 +2,7 @@ $(function() {
 
 	$('.wrapper').carousel({
 
-	// 	leftBtn : '.carousel-arrow-left',
-	//  rightBtn : '.carousel-arrow-right',
-	//  autoPlay: false, //true or false
-	//  delay: 1000
+	
 	});
 
 
