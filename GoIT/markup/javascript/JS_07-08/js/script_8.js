04a63d4630052c62e0aeb858955f98ad6b603c1f
Anna Mikhaylova <acidity84@gmail.com>
2016-09-19 13:54:08 +0300

    comment mentor 7-8

--- GoIT/markup/javascript/JS_07-08/js/script_8.js
+++ GoIT/markup/javascript/JS_07-08/js/script_8.js
@@ -12,36 +12,25 @@ $(function() {
     var numberOfTab = '.tabText' + number;
     $('.tabText p').hide();
     $(numberOfTab).show();
-  });
-
-
-  $("input[name='firstName']").on('mouseover', function() {
-    $(".showField1").show();
-    // $(".showField1").animate({
-		// 	opasity: 1
-		// }, 1000, function() {
-		// 	$(".showField1").show();
-		// });
 
   });
-  $("input[name='firstName']").on('mouseleave', function() {
-    $('.showField1').hide();
-  });
-  $("input[name='lastName']").on('mouseover', function() {
-    $('.showField2').show();
-  });
-  $("input[name='lastName']").on('mouseleave', function() {
-    $('.showField2').hide();
-  });
-  $("input[name='address']").on('mouseover', function() {
-    $('.showField3').show();
-  });
-  $("input[name='address']").on('mouseleave', function() {
-    $('.showField3').hide();
-  });
+
+// ---------------------------------
+
+
+ $('label').append('<span>'+$('input').attr('title')+'</span>');
+
+ $('input').hover(function () {
+   $(this).siblings('span').css('opacity', '1');
+   var titleText = $(this).attr('title');
+   $(this).siblings('span').text(titleText);
+ }, function() {
+      $(this).siblings('span').css('opacity', '0');
+ });
+
 
   $('.showButton').on('click', function() {
-    $('.show').show();
+    $('span').show().css('opacity', '1');
   });
 
 });
