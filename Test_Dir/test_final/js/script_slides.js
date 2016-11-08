9d535677af4a619f5e8ed0dca802a6302fa2badd
Anna Mikhaylova <acidity84@gmail.com>
2016-11-09 01:19:47 +0200

    added test_final_ with plugin

---
+++ Test_Dir/test_final/js/script_slides.js
@@ -0,0 +1,26 @@
+$(function () {
+	$("#slides1").responsiveSlides({
+		auto: false,
+		pagination: true,
+		nav: true,
+		fade: 500,
+		// maxwidth: 800
+	});
+
+	$("#slides2").responsiveSlides({
+		auto: false,
+		pagination: true,
+		nav: true,
+		fade: 500,
+		// maxwidth: 800
+	});
+
+	$("#slides3").responsiveSlides({
+		auto: false,
+		pagination: true,
+		nav: true,
+		fade: 500,
+		// maxwidth: 800
+	});
+
+});
