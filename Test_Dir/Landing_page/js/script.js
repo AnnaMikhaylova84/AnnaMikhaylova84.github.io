beb5a22b9b32b70cffa6e704938e098504037d80
AnnaMikhaylova84 <user.name>
2016-12-13 02:36:40 +0200

    added new js_files

---
+++ Test_Dir/Landing_page/js/script.js
@@ -0,0 +1,83 @@
+jQuery(document).ready(function() {
+  jQuery('.flip').addClass('hidden').viewportChecker({
+   classToAdd: 'visible animated flip',
+   offset: 20
+  });
+});
+
+jQuery(document).ready(function() {
+  jQuery('.fadeInLeft').addClass('hidden').viewportChecker({
+   classToAdd: 'visible animated fadeInLeft',
+   offset: 20
+  });
+});
+
+jQuery(document).ready(function() {
+  jQuery('.fadeInRight').addClass('hidden').viewportChecker({
+   classToAdd: 'visible animated fadeInRight',
+   offset: 20
+  });
+});
+
+jQuery(document).ready(function() {
+  jQuery('.fadeInUp').addClass('hidden').viewportChecker({
+   classToAdd: 'visible animated fadeInUp',
+   offset: 20
+  });
+});
+
+jQuery(document).ready(function() {
+  jQuery('.fadeInDown').addClass('hidden').viewportChecker({
+   classToAdd: 'visible animated fadeInDown',
+   offset: 20
+  });
+});
+
+jQuery(document).ready(function() {
+  jQuery('.rotateLeft').addClass('hidden').viewportChecker({
+   classToAdd: 'visible animated rotateInDownLeft',
+   offset: 100
+  });
+});
+
+jQuery(document).ready(function() {
+  jQuery('.rotateRight').addClass('hidden').viewportChecker({
+   classToAdd: 'visible animated rotateInDownRight',
+   offset: 20
+  });
+});
+
+jQuery(document).ready(function() {
+  jQuery('.rollIn').addClass('hidden').viewportChecker({
+   classToAdd: 'visible animated rollIn',
+   offset: 20
+  });
+});
+
+jQuery(document).ready(function() {
+  jQuery('.slideInLeft').addClass('hidden').viewportChecker({
+   classToAdd: 'visible animated slideInLeft',
+   offset: 20
+  });
+});
+
+jQuery(document).ready(function() {
+  jQuery('.rubberBand').addClass('hidden').viewportChecker({
+   classToAdd: 'visible animated rubberBand',
+   offset: 20
+  });
+});
+
+jQuery(document).ready(function() {
+  jQuery('.zoomInUp').addClass('hidden').viewportChecker({
+   classToAdd: 'visible animated zoomInUp',
+   offset: 20
+  });
+});
+
+jQuery(document).ready(function() {
+  jQuery('.zoomInDown').addClass('hidden').viewportChecker({
+   classToAdd: 'visible animated zoomInDown',
+   offset: 20
+  });
+});
