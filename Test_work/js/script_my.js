3eada69798d094c83af442b099d039cce8256c52
AnnaMikhaylova84 <user.name>
2016-12-13 14:13:02 +0200

    added cheeves

---
+++ Test_work/js/script_my.js
@@ -0,0 +1,41 @@
+jQuery(document).ready(function() {
+  jQuery('.fadeInLeft').addClass('hidden').viewportChecker({
+   classToAdd: 'visible animated fadeInLeft',
+   offset: 100
+  });
+});
+
+jQuery(document).ready(function() {
+  jQuery('.fadeInRight').addClass('hidden').viewportChecker({
+   classToAdd: 'visible animated fadeInRight',
+   offset: 100
+  });
+});
+
+jQuery(document).ready(function() {
+  jQuery('.fadeInDown').addClass('hidden').viewportChecker({
+   classToAdd: 'visible animated fadeInDown',
+   offset: 100
+  });
+});
+
+jQuery(document).ready(function() {
+  jQuery('.fadeInUp').addClass('hidden').viewportChecker({
+   classToAdd: 'visible animated fadeInUp',
+   offset: 50
+  });
+});
+
+jQuery(document).ready(function() {
+  jQuery('.zoomInUp').addClass('hidden').viewportChecker({
+   classToAdd: 'visible animated zoomInUp',
+   offset: 50
+  });
+});
+
+jQuery(document).ready(function() {
+  jQuery('.zoomInDown').addClass('hidden').viewportChecker({
+   classToAdd: 'visible animated zoomInDown',
+   offset: 50
+  });
+});
