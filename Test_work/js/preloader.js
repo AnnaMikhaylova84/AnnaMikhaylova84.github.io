e27fce322a150cb1570e060791172120d49d5d56
AnnaMikhaylova84 <user.name>
2016-12-21 16:20:07 +0200

    added cheeves files

--- Test_work/js/preloader.js
+++ Test_work/js/preloader.js
@@ -1,6 +1,6 @@
 $(window).on('load', function () {
 	var $preloader = $('#circular'),
 	$svg_anm   = $preloader.find('.circularG');
-	$svg_anm.delay(3000).fadeOut(1500);
-	$preloader.delay(2000).fadeOut(2500);
+	$svg_anm.delay(3000).fadeOut(1500);   // animation
+	$preloader.delay(2000).fadeOut(2500);  // fon
 });
