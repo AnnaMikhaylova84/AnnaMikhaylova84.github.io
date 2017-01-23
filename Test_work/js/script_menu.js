1a86bf9a76036865c6ee9f3b21feccefdf15746a
AnnaMikhaylova84 <user.name>
2017-01-23 15:32:27 +0200

    added js-menu

---
+++ Test_work/js/script_menu.js
@@ -0,0 +1,24 @@
+// function hey() {
+//   alert('hey-hey!');
+// }
+//
+// hey();
+
+var dropElem = document.getElementById('dropmenu');
+console.log('dropElem', dropElem);
+  var titleElem = dropElem.querySelector('.droplink');
+  console.log('titleElem', titleElem);
+  var dropDown = dropElem.querySelector('.header_new__dropdown');
+  console.log('dropDown', dropDown);
+var close = document.getElementById('close');
+console.log('close', close);
+
+titleElem.onclick = function() {
+  dropDown.classList.toggle('open');
+  console.log('dropDown after+open', dropDown);
+};
+
+close.onclick = function() {
+  dropDown.classList.toggle('open');
+  console.log('dropDown after+open', dropDown);
+}
