b499e8cdc25c60f51cbb050399966bc3f6554cad
AnnaMikhaylova84 <user.name>
2017-01-27 00:48:11 +0200

    animation without hidden

--- Test_work/js/script_my.js
+++ Test_work/js/script_my.js
@@ -3,47 +3,47 @@
 //    	if (jQuery(window).width() > 960) document.write ('<link rel="stylesheet" href="css/animate.css">');
 // })
 
-jQuery(document).ready(function() {
-  jQuery('.fadeInLeft').addClass('hidden').viewportChecker({
-   classToAdd: 'visible animated fadeInLeft',
-   offset: 100
-  });
-});
-
-jQuery(document).ready(function() {
-  jQuery('.fadeInRight').addClass('hidden').viewportChecker({
-   classToAdd: 'visible animated fadeInRight',
-   offset: 100
-  });
-});
-
-jQuery(document).ready(function() {
-  jQuery('.fadeInDown').addClass('hidden').viewportChecker({
-   classToAdd: 'visible animated fadeInDown',
-   offset: 100
-  });
-});
-
-jQuery(document).ready(function() {
-  jQuery('.fadeInUp').addClass('hidden').viewportChecker({
-   classToAdd: 'visible animated fadeInUp',
-   offset: 100
-  });
-});
-
-jQuery(document).ready(function() {
-  jQuery('.zoomInUp').addClass('hidden').viewportChecker({
-   classToAdd: 'visible animated zoomInUp',
-   offset: 100
-  });
-});
-
-jQuery(document).ready(function() {
-  jQuery('.zoomInDown').addClass('hidden').viewportChecker({
-   classToAdd: 'visible animated zoomInDown',
-   offset: 100
-  });
-});
+// jQuery(document).ready(function() {
+//   jQuery('.fadeInLeft').addClass('hidden').viewportChecker({
+//    classToAdd: 'visible animated fadeInLeft',
+//    offset: 100
+//   });
+// });
+//
+// jQuery(document).ready(function() {
+//   jQuery('.fadeInRight').addClass('hidden').viewportChecker({
+//    classToAdd: 'visible animated fadeInRight',
+//    offset: 100
+//   });
+// });
+//
+// jQuery(document).ready(function() {
+//   jQuery('.fadeInDown').addClass('hidden').viewportChecker({
+//    classToAdd: 'visible animated fadeInDown',
+//    offset: 100
+//   });
+// });
+//
+// jQuery(document).ready(function() {
+//   jQuery('.fadeInUp').addClass('hidden').viewportChecker({
+//    classToAdd: 'visible animated fadeInUp',
+//    offset: 100
+//   });
+// });
+//
+// jQuery(document).ready(function() {
+//   jQuery('.zoomInUp').addClass('hidden').viewportChecker({
+//    classToAdd: 'visible animated zoomInUp',
+//    offset: 100
+//   });
+// });
+//
+// jQuery(document).ready(function() {
+//   jQuery('.zoomInDown').addClass('hidden').viewportChecker({
+//    classToAdd: 'visible animated zoomInDown',
+//    offset: 100
+//   });
+// });
 
 
 
