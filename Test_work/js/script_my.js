e671fda2c0c74ac27c9161921f98c3ff864c9b1a
AnnaMikhaylova84 <user.name>
2017-01-26 12:09:04 +0200

    updated date

--- Test_work/js/script_my.js
+++ Test_work/js/script_my.js
@@ -1,60 +1,13 @@
-// if (jQuery(window).width() > 1200) {
-
-var windowWidth = $(window).width();
-console.log(windowWidth);
-
-// jQuery(document).ready(function() {
-//   jQuery('.fadeInLeft').addClass('hidden').viewportChecker({
-//    classToAdd: 'visible animated fadeInLeft',
-//    offset: 100
-//   });
-// });
-//
-// jQuery(document).ready(function() {
-//   jQuery('.fadeInRight').addClass('hidden').viewportChecker({
-//    classToAdd: 'visible animated fadeInRight',
-//    offset: 100
-//   });
-// });
-//
-// jQuery(document).ready(function() {
-//   jQuery('.fadeInDown').addClass('hidden').viewportChecker({
-//    classToAdd: 'visible animated fadeInDown',
-//    offset: 100
-//   });
-// });
-//
-// jQuery(document).ready(function() {
-//   jQuery('.fadeInUp').addClass('hidden').viewportChecker({
-//    classToAdd: 'visible animated fadeInUp',
-//    offset: 100
-//   });
-// });
-//
-// jQuery(document).ready(function() {
-//   jQuery('.zoomInUp').addClass('hidden').viewportChecker({
-//    classToAdd: 'visible animated zoomInUp',
-//    offset: 100
-//   });
-// });
-//
-// jQuery(document).ready(function() {
-//   jQuery('.zoomInDown').addClass('hidden').viewportChecker({
-//    classToAdd: 'visible animated zoomInDown',
-//    offset: 100
-//   });
-// });
-
-// }
 
 $( function() {
   $( "#datepicker" ).datepicker({
-    // altField: 'toLocaleDateString()',
     dateFormat: 'dd/mm/yy',
     duration: 'slow',
     maxDate: '+6m',
     minDate: '0d',
 
   });
+  $("#datepicker" ).datepicker("setDate" , "0");
+
 
 } );
